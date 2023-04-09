872f11aaf7f5d42816d270f5e1ee4e1c0f89e270
식 <entlrsla@gmail.com>
2023-04-09 16:45:22 +0900

    solution4 풀이....근데 프로그래머스 test에서 걸림 이유를 잘 모르겠음

--- src/solution/Solution4.java
+++ src/solution/Solution4.java
@@ -2,14 +2,55 @@ package solution;
 
 public class Solution4 {
 	//신규아이디 추천
-	String new_id = "...!@BaT#*..y.abcdefghijklm";
+	String new_id = "=.=";
 	public String solution4() {
         String answer = "";
-        String regEx = "[a-z0-9-_.]";
-        System.out.println(new_id);
-        System.out.println(new_id.toLowerCase());
-        System.out.println(answer.replace(regEx, ""));
         
-        return answer;
+        System.out.println(new_id);
+        //1단계
+        new_id = new_id.toLowerCase();
+        System.out.println(new_id);
+        //2단계
+        new_id = new_id.replaceAll("[~!@#$%\\\\^&*()=+\\\\[\\\\{\\\\]\\\\}:?,<>/]", "");
+        System.out.println(new_id);
+        //3단계
+        new_id = new_id.replaceAll("\\.{2,}", ".");
+      	System.out.println(new_id);
+      	//4단계
+//      	if(!(new_id.length() == 0)) {
+//      		if(new_id.indexOf(".") == 0) {
+//          		new_id = new_id.substring(1);
+//          	}
+//      		if(!(new_id.length() == 0)){
+//      			if(new_id.lastIndexOf(".") == new_id.length()-1) {
+//              		new_id = new_id.substring(0, new_id.length() - 1);
+//              	}
+//      		}
+//          	
+//      	}
+      	new_id = new_id.replaceAll("^[.]|[.]$", "");
+      	System.out.println(new_id);
+      	//5단계
+      	if(new_id.length() == 0) {
+      		new_id = "a";
+      	}
+      	System.out.println(new_id);
+      	//6단계
+      	if(new_id.length() > 15) {
+      		new_id = new_id.substring(0, 15);
+      		new_id = new_id.replaceAll("[.]$", "");
+      	}
+      	System.out.println(new_id);
+      	System.out.println(new_id.length());
+      	//7단계
+      	if(new_id.length() <= 2) {
+			for(int i = new_id.length(); i < 3; i++) {
+				new_id = new_id + new_id.charAt(new_id.length()-1);
+      		}
+      	}
+      	System.out.println(new_id);
+      	System.out.println(new_id.length());
+      	answer = new_id;
+      	return answer;
     }
 }
